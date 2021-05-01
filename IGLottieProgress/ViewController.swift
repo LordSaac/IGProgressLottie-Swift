68488de054226aa9cfc08c4cfa6acd44068f4a9b
isaacgutierrez <jose19.26@>
2021-05-01 11:29:47 -0500

    Initial Commit

---
+++ IGLottieProgress/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IGLottieProgress
+//
+//  Created by Isaac Gutiérrez on 05/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
